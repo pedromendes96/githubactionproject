f51de8549ec37b5e675321dacf1907574d2920d2
Pedro Mendes <pedro.trabalho.uma@gmail.com>
2019-11-24 23:43:09 +0000

    dsa

--- app/manage.py
+++ app/manage.py
@@ -5,6 +5,7 @@ import sys
 
 
 def main():
+    # s
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
     try:
         from django.core.management import execute_from_command_line
